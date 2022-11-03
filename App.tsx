cd64677796814865b6c93cc8a0aee9c78fcea4b0
Emerson Laverty <emersonlaverty@Emersons-MacBook-Pro-3.local>
2022-11-02 20:50:48 -0400

    adding app changes to sign up

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import SignUp from "./SignUp";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi VOID!</Text>
+    <SignUp />
       <StatusBar style="auto" />
     </View>
   );
